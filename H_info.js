1eb30b720947f5746c1cbfd659c2b92b6d6418a4
WU.XUHUI <44720404+Best-Hotel-Japan@users.noreply.github.com>
2019-08-30 21:10:25 +0900

    New Hotel Info

---
+++ H_info.js
@@ -0,0 +1,15 @@
+Hotel_information: {                            // HnameTable酒店资料数组初始化
+
+    var HnameTable = new Array();
+    HnameTable["未知酒店"]=["未知酒店名","未知Cid","未知","未知Jid","en",[""]];
+
+
+}
+
+Hotel_RoomType_information: {                   // 各酒店房型资料数组初始化
+
+    HnameTable["未知酒店"][5]["舒适日式房"]=["000088600","04禁，05"];      // 满足顺序查找
+    HnameTable["未知酒店"][5]["日式房"]=["000188201","07禁，08"];               
+
+
+}
